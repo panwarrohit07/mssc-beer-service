aac63253806b11630019fb0599ef9d5b1ca247ec
panwarrohit07 <panwarrohit07@gmail.com>
2022-10-01 19:52:44 +0530

    Adding beer entity. Closes #5

--- src/main/java/com/udemy/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/udemy/msscbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
@@ -15,17 +16,24 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
 
         //to do
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(BeerDto.builder().beerName("Hoegarden").price(BigDecimal.valueOf(200)).build(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
         //todo impl
-        return new ResponseEntity(HttpStatus.CREATED);
+        BeerDto beerDto1 = BeerDto.builder().id(UUID.randomUUID()).build();
+        return new ResponseEntity(beerDto1, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity(BeerDto.builder().id(UUID.randomUUID()).build(), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteBeerById(@PathVariable("beerId") UUID beerId){
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }

373ab8335f18b4b37e69f2556b0a26a4cae3d14f
panwarrohit07 <panwarrohit07@gmail.com>
2022-10-01 19:57:02 +0530

    Adding beer repository. Closes #6

---
+++ src/main/java/com/udemy/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.udemy.msscbeerservice.repositories;
+
+import com.udemy.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
